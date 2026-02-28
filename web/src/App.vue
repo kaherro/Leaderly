f6eb67a2076e44de2cad75183159285ed6d4023b
Dũng <94789481+letkh@users.noreply.github.com>
2026-02-28 18:59:28 +0300

    [feat]: add vue.js structure

---
+++ web/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Welcome to Leaderly!')
+</script>
+
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <p>vue project is ready!</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
