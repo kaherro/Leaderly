e1e1ded7c966059c88f05f114f78a03466f426ee
kaherro <torkomyanvv@gmail.com>
2026-03-03 18:56:21 +0300

    [feat]: Added postgresql queries

--- src/bases/db_manager.py
+++ src/bases/db_manager.py
@@ -3,11 +3,12 @@ This module defines the DBManager class,
 which provides methods to interact with a Redis database.
 '''
 import redis
+import psycopg2
 import logging
 
 logging.basicConfig(
     level=logging.INFO, 
-    format='%(asctime)s - [%(levelname)s] - %(name)s - %(message)s',
+    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
@@ -19,21 +20,67 @@ class DBManager:
         self.host = host
         self.port = port
         self._connect()
+        self.__init_postgres()
         
     def _connect(self):
-        '''Connects to the Redis database and handles connection errors.'''
+        '''Connects to the Redis and PostgreSQL databases and handles connection errors.'''
         try:
-            self.r = redis.Redis(host=self.host, port=self.port, decode_responses=True)
-            self.r.ping()
+            self.redis_db = redis.Redis(host=self.host, port=self.port, decode_responses=True)
+            self.redis_db.ping()
             logger.info("Connected to Redis successfully!")
         except redis.exceptions.ConnectionError as e:
             logger.error(f"Failed to connect to Redis: {e}")
-            self.r = None
+            self.redis_db = None
 
-    def refresh_score(self, key: str, score: float):
+        try:
+            self.pg_db = psycopg2.connect(
+                dbname="leaderly", 
+                user="postgres", 
+                password="2991", 
+                host="127.0.0.1", 
+                port="5432"
+            )
+            logger.info("Connected to PostgreSQL successfully!")
+        except psycopg2.Error as e:
+            logger.error(f"Failed to connect to PostgreSQL: {e}")
+            self.pg_db = None
+
+    def __init_postgres(self):
+        '''Initializes the PostgreSQL database with the necessary table.'''
+        if self.pg_db is None:
+            logger.error("PostgreSQL connection not established.")
+            return
+        
+        try:
+            cursor = self.pg_db.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS leaderboard (
+                    id SERIAL PRIMARY KEY,
+                    key VARCHAR(255) UNIQUE NOT NULL,
+                    score FLOAT NOT NULL,
+                    tags INTEGER[]
+                );
+            ''')
+            self.pg_db.commit()
+            cursor.close()
+            logger.info("PostgreSQL database initialized successfully!")
+        except psycopg2.Error as e:
+            logger.error(f"Error initializing PostgreSQL database: {e}")
+            if self.pg_db:
+                self.pg_db.rollback()
+
+    def update_score(self, key: str, score: float):
         '''Updates the score for a given key in the leaderboard.'''
         try:
-            self.r.zadd('leaderboard', {key: score})
+            self.redis_db.zadd('leaderboard', {key: score})
+            cur = self.pg_db.cursor()
+            cur.execute(f'''
+                INSERT INTO leaderboard (key, score) 
+                VALUES ('{key}', {score}) 
+                ON CONFLICT (key) 
+                DO UPDATE SET score = {score};
+            ''')
+            self.pg_db.commit()
             logger.info(f"Updated {key}'s score to {score}.")
         except Exception as e:
             logger.error(f"Error updating score: {e}")
@@ -41,7 +88,7 @@ class DBManager:
     def get_score(self, key: str):
         '''Returns the score for a given key from the leaderboard.'''
         try:
-            score = self.r.zscore('leaderboard', key)
+            score = self.redis_db.zscore('leaderboard', key)
             if score is not None:
                 logger.info(f"{key}'s score: {score}")
                 return score
@@ -52,24 +99,30 @@ class DBManager:
             logger.error(f"Error retrieving score: {e}")
             return None
 
-    def get_leaderboard(self, n:int=0):
+    def get_leaderboard(self, n:int = 0):
         '''Returns the top N objects from the leaderboard.
         If n is 0 or not given, returns the entire leaderboard.'''
         try:
-            leaderboard = self.r.zrange('leaderboard', 0, n - 1, withscores=True)
+            leaderboard = self.redis_db.zrange('leaderboard', 0, n - 1, withscores=True)
             logger.info(f"Top {n} Leaderboard: {leaderboard}")
             return leaderboard
         except Exception as e:
             logger.error(f"Error retrieving leaderboard: {e}")
             return []
 
-    def refresh_tags(self, key:str, tags:list):
+    def update_tags(self, key:str, tags:list):
         ''''Updates the tags for a given key. 
         If tags is empty, it clears all tags for that key.'''
         try:
-            self.r.delete(key)
+            self.redis_db.delete(key)
+            cur = self.pg_db.cursor()
+            cur.execute(f'''
+                UPDATE leaderboard 
+                SET tags = ARRAY{tags} 
+                WHERE key = '{key}';
+            ''')
             if tags:
-                self.r.lpush(key, *tags)
+                self.redis_db.lpush(key, *tags)
                 logger.info(f"Updated {key}'s tags: {tags}")
             else:
                 logger.info(f"Cleared all tags for {key}.")
@@ -79,8 +132,13 @@ class DBManager:
     def delete_object(self, key:str):
         '''Deletes a given key from the leaderboard and its associated tags.'''
         try:
-            self.r.zrem('leaderboard', key)
-            self.r.delete(key)
+            self.redis_db.zrem('leaderboard', key)
+            self.redis_db.delete(key)
+            cur = self.pg_db.cursor()
+            cur.execute(f'''
+                DELETE FROM leaderboard WHERE key = '{key}';
+            ''')
+            self.pg_db.commit()
             logger.info(f"Deleted {key} from leaderboard.")
         except Exception as e:
             logger.error(f"Error deleting user: {e}")
