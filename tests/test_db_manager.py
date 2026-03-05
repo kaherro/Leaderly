63b0a7781aca1a873880ad5c1d6428ec24439bd2
kaherro <torkomyanvv@gmail.com>
2026-03-05 03:52:31 +0300

    [test]: Added tests

--- tests/test_db_manager.py
+++ tests/test_db_manager.py
@@ -4,15 +4,13 @@ import psycopg2
 from src.bases.db_manager import DBManager
 
 class TestDBManager(unittest.TestCase):
-    def test_1(self):
-        self.assertEqual('1','1')
+    def setUp(self):
+        self.db = DBManager()
+        self.db.redis_db.flushdb()
+        with self.db.pg_db.cursor() as cur:
+            cur.execute("DELETE FROM leaderboard;")
+            self.db.pg_db.commit()
 
-    def test_initialization_success(self):
-        db = DBManager()
-        self.assertIsNotNone(db.redis_db)
-        self.assertIsNotNone(db.pg_db)
-
-    
     def test_initialization_redis(self):
         with self.assertRaises(AttributeError):  
             db = DBManager(redis_port=99523)
@@ -34,4 +32,40 @@ class TestDBManager(unittest.TestCase):
             db = DBManager(pg_user='adsvn')
 
         with self.assertRaises(AttributeError):  
-            db = DBManager(pg_dbname='agnkas')
+            db = DBManager(pg_dbname='agnkas')    
+    
+    def test_update_score_new_user(self):
+        self.db.update_score("test_user", 100)
+        score = self.db.get_score("test_user")
+        self.assertEqual(score, 100)
+    
+    def test_update_score_existing_user(self):
+        self.db.update_score("test_user", 100)
+        self.db.update_score("test_user", 200)
+        score = self.db.get_score("test_user")
+        self.assertEqual(score, 200)
+    
+    def test_update_score_multiple_users(self):
+        db = DBManager()
+        users = {"user1": 100, "user2": 200, "user3": 300}
+        for user, score in users.items():
+            self.db.update_score(user, score)
+        
+        for user, expected_score in users.items():
+            score = self.db.get_score(user)
+            self.assertEqual(score, expected_score)
+    
+    def test_get_score_nonexistent_user(self):
+        score = self.db.get_score("nonexistent")
+        self.assertIsNone(score)
+
+    def test_get_leaderboard_all(self):
+        scores = [("user1", 300), ("user2", 200), ("user3", 100)]
+        for user, score in scores:
+            self.db.update_score(user, score)
+        leaderboard = self.db.get_leaderboard(0)
+        self.assertEqual(len(leaderboard), 3)
+    
+    def test_get_leaderboard_empty(self):
+        leaderboard = self.db.get_leaderboard(10)
+        self.assertEqual(leaderboard, [])
